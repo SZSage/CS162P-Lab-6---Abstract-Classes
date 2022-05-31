58a9a2ea21187c1165931b5d8bf178d0aef266b2
Simon Zhao <zhaos2@my.lanecc.edu>
2022-05-30 17:42:55 -0700

    Final

--- MexicanTrainDominos/mexicanTrain.py
+++ MexicanTrainDominos/mexicanTrain.py
@@ -6,8 +6,9 @@ class MexicanTrain(Train):
     """ this class represents the mexican train.  It is derived from the abstract Train class.
     It MUST implement the abstract method isPlayable. """
 
-    def __init__(self):
+    def __init__(self, engineValue=12):
         super().__init__()
+        self._engineValue = engineValue
 
     def isPlayable(self, hand, domino):
         return self._isPlayable(domino)
--- MexicanTrainDominos/playerTrain.py
+++ MexicanTrainDominos/playerTrain.py
@@ -7,17 +7,18 @@ class PlayerTrain(Train):
     It MUST implement the abstract method isPlayable. It has 2 new instance variables --
     the hand(player) to which it belongs and a flag that determines if the train is currently open """
 
-    def __init__(self, hand, isOpen=False):
+    def __init__(self, hand, isOpen=False, engineValue=12):
         super().__init__()
         self.__hand = hand
         self.__isOpen = isOpen
+        self._engineValue = engineValue
 
     @property
     def isOpen(self):
-        return self.__isOpen == self.__isOpen
+        return self.__isOpen
 
     def isPlayable(self, hand, domino):
-        if Hand() == self.__hand or self.__isOpen:
+        if hand == self.__hand or self.__isOpen:
             return self._isPlayable(domino)
 
     def open(self):
